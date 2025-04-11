3974c67a566ac71f81971bb5355b239a17598aa3
LeninCar <lenincarabali@gmail.com>
2025-04-11 15:37:29 -0500

    Finalizado

--- sudoku/testGenerator.py
+++ sudoku/testGenerator.py
@@ -9,8 +9,8 @@ import tempfile
 # Configuración
 # ======================
 MINIZINC_PATH = r"C:\Program Files\MiniZinc\minizinc.exe"
-MODEL_FILE = "sudoku-test.mzn"
-TEST_FOLDER = "tests"
+MODEL_FILE = "sudoku-test-redun.mzn"
+TEST_FOLDER = "tests-redun"
 
 # Estrategias a probar
 strategies = [
@@ -80,13 +80,13 @@ for dzn_file in os.listdir(TEST_FOLDER):
                     solve_line = "\nsolve satisfy;\n"
                 else:
                     solve_line = f"""
-solve :: int_search(
-  [tabla[i, j] | i in 1..9, j in 1..9],
-  {var_heur},
-  {val_heur},
-  complete
-) satisfy;
-"""
+                    solve :: int_search(
+                    [tabla[i, j] | i in 1..9, j in 1..9],
+                    {var_heur},
+                    {val_heur},
+                    complete
+                    ) satisfy;
+                    """
 
                 # Crear archivo temporal con la línea solve
                 with tempfile.NamedTemporaryFile(mode="w", suffix=".mzn", delete=False, encoding="utf-8") as tmp_file:
@@ -133,7 +133,7 @@ solve :: int_search(
 # ======================
 # Guardar archivo Excel
 # ======================
-output_file = "resultados.xlsx"
+output_file = "resultados-redun.xlsx"
 workbook.save(output_file)
 print(f"Resultados guardados en {output_file}")
 
